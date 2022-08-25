b98247c83508587d32a24126108351c8ef701ae1
João Carlos <107141025+joaocarlosrj@users.noreply.github.com>
2022-08-25 17:48:11 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+function carregar() {
+var msg = window.document.getElementById('msg')
+var img = window.document.getElementById('imagem')
+var data = new Date()
+var hora = data.getHours()
+
+msg.innerHTML = `Agora são ${hora} horas`
+if (hora >= 0 && hora < 12){
+    // Bom dia
+    img.src = 'manha.png'
+    document.body.style.background = '#599d9d'
+} else if (hora >= 12 && hora < 18){
+// Boa tarde
+document.body.style.background = '#de5202'
+img.src = 'tarde.png'
+} else {
+    // Boa noite
+    img.src = 'noite.png'
+    document.body.style.background = '#111a21'
+}
+}
